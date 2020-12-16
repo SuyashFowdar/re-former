e40d31876c946d5db0403631bd92bd070bba923d
Suyash <suyash.krishna9@gmail>
2020-12-16 17:17:39 +0400

    Create form for new user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  include UsersHelper
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+end

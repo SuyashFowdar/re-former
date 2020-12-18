094a20b0cb40874a01bbaf13edda3a48495f84cf
Suyash <suyash.krishna9@gmail>
2020-12-18 12:03:57 +0400

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :username, :password, :email, presence: true
   validates :username, uniqueness: { message: 'An account associated with %<value>s already exists' }
   validates :password, length: { in: 4..12 }
+  validates_format_of :email, :with => /^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/i , :multiline => true
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 end
